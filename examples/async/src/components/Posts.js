8d9cd5392ad894b5d1d09df0c932733fb6143261
NelsonKuang <410374308@qq.com>
2017-08-30 12:16:13 +0800

    '优化异步例子中CNODE社区的贴子的显示'

--- examples/async/src/components/Posts.js
+++ examples/async/src/components/Posts.js
@@ -6,7 +6,9 @@ const Posts = ({posts}) => (
     {posts.map((post, i) =>
     	<li key={i}>
     		{post.author.avatar_url && <img src={post.author.avatar_url} style={{width:'50px',height:'50px',borderRadius:'50%'}} />}
-    		<a href={post.url} target="_blank">{post.title}</a>
+    		{post.top && <span className="top">[置顶]</span>}
+    		{post.title}
+    		<span className="time">{new Date(post.create_at).toLocaleDateString() + ' ' + new Date(post.create_at).toLocaleTimeString()}</span>
     	</li>
     )}
   </ul>
